d8e46e7cbe3091d7c3b4bac07932efc238d1e4d7
ontu001 <mdrohanurrahmanontu@gmail.com>
2026-01-14 00:05:11 +0600

    basic calculator

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// Basic stuff
-
-// let a = parseInt(prompt("Enter 1st number: "))
-// let b = parseInt(prompt("Enter 2nd number: "))
-// let c = prompt("Choose your operator between +, -, *, /")
-
-// if (c == '+'){
-//     console.log(`${a} + ${b} = ${a+b}`)
-// }
-// else if(c == '-'){
-//     console.log(`${a} - ${b} = ${a-b}`)
-// }
-// else if( c == '*'){
-//     console.log(`${a} x ${b} = ${a*b}`)
-// }
-// else if(c == '/'){
-//     console.log(`${a} / ${b} = ${a/b}`)
-// }
-// else{
-//     console.log(`${c} Is Invalid Operator`)
-// }
-
-
 
 //---- Array
 // const topAiTool=['OpenAI','Gemini','Cluade','Grok','LLMA'];
