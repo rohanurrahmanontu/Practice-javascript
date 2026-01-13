c28a219e845d01609021a5964af9b2f6636e4ae2
ontu001 <mdrohanurrahmanontu@gmail.com>
2026-01-14 00:42:10 +0600

    find max number

---
+++ find_max_num.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 12
+let c = 19
+
+if (a>b){
+    console.log(a)
+}
+else if(b>c){
+    console.log(b)
+}
+else{
+   console.log(c) 
+}
+
--- reverse_string.js
+++ reverse_string.js
@@ -1,8 +1,19 @@
-let a = prompt("Enter your word to reverse:");
+// let a = prompt("Enter your word to reverse:");
+// let reversed = "";
+
+// for (let i = a.length-1; i >= 0; i--) {
+//     reversed += a[i]; 
+// }
+
+// console.log(reversed);
+
+
+// modern way
+let a = prompt("Enter your word:");
 let reversed = "";
 
-for (let i = a.length-1; i >= 0; i--) {
-    reversed += a[i]; 
+for (let char of a) {
+    reversed = char + reversed;
 }
 
 console.log(reversed);
